debb46aa006145f61e54186179b58d4e813b2944
slash585 <slsh585@gmail.com>
2024-04-11 17:24:48 +0300

    Init app

---
+++ RickAndMorty/Scene/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Mehmet Ali Özdemir on 11.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemMint
+    }
+
+
+}
+

db6ce839dda1e0199644a4531b3d4385106a7e18
slash585 <slsh585@gmail.com>
2024-08-30 15:15:38 +0300

    Add activity indicator

--- RickAndMorty/RickAndMorty/Modules/Characters/CharactersViewController.swift
+++ RickAndMorty/RickAndMorty/Modules/Characters/CharactersViewController.swift
@@ -6,17 +6,55 @@
 //
 
 import UIKit
+import SnapKit
 
-protocol CharactersViewControllerProtocol: AnyObject { }
+protocol CharactersViewControllerProtocol: AnyObject {
+    func activityIndicatorStart()
+    func activityIndicatorStop()
+}
 
 final class CharactersViewController: UIViewController {
 
     var viewModel: CharactersViewModelProtocol!
+    
+    private lazy var activityIndicator: UIActivityIndicatorView = {
+        let activityIndicator = UIActivityIndicatorView(style: .large)
+        return activityIndicator
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         viewModel.viewDidLoad()
+        configureUI()
     }
 }
 
-extension CharactersViewController: CharactersViewControllerProtocol { }
+extension CharactersViewController {
+    private func configureUI(){
+        view.backgroundColor = .white
+        
+        makeActivityIndicator()
+    }
+    
+    private func makeActivityIndicator(){
+        view.addSubview(activityIndicator)
+        
+        activityIndicator.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+}
+
+extension CharactersViewController: CharactersViewControllerProtocol {
+    func activityIndicatorStart() {
+        DispatchQueue.main.async {
+            self.activityIndicator.startAnimating()
+        }
+    }
+    
+    func activityIndicatorStop() {
+        DispatchQueue.main.async {
+            self.activityIndicator.stopAnimating()
+        }
+    }
+}
--- RickAndMorty/RickAndMorty/Modules/Characters/CharactersViewModel.swift
+++ RickAndMorty/RickAndMorty/Modules/Characters/CharactersViewModel.swift
@@ -8,6 +8,8 @@
 import Foundation
 
 protocol CharactersViewModelProtocol {
+    var characters: RMGetAllCharactersResponse? { get set }
+    
     func viewDidLoad()
 }
 
@@ -16,12 +18,33 @@ final class CharactersViewModel: CharactersViewModelProtocol {
     private weak var view: CharactersViewControllerProtocol?
     private let service: NetworkManagerProtocol?
     
+    var characters: RMGetAllCharactersResponse?
+    
     init(view: CharactersViewControllerProtocol?, service: NetworkManagerProtocol) {
         self.service = NetworkManager.shared
         self.view = view
     }
     
     func viewDidLoad() {
-        print("worked")
+        fetchCharacters()
+    }
+}
+
+extension CharactersViewModel {
+    private func fetchCharacters() {
+        view?.activityIndicatorStart()
+        service?.request(.getCharacters, completion: { [weak self] (result: (Result<RMGetAllCharactersResponse, Error>)) in
+            
+            guard let self else { return }
+            self.view?.activityIndicatorStop()
+            switch result {
+            case .success(let data):
+                DispatchQueue.main.async {
+                    self.characters = data
+                }
+            case .failure(let error):
+                print(error)
+            }
+        })
     }
 }
